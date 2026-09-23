bd3495e0761a4a34a74d3ca07da1a59c420b5e65
sana ullah <sunnykhansadda@gmail.com>
2026-09-23 13:48:31 +0500

    added llm gemini key and groq also making route fo this

--- backend/services/agent/config/llmodels.js
+++ backend/services/agent/config/llmodels.js
@@ -1,4 +1,5 @@
 import { ChatGroq } from "@langchain/groq";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 const groq = new ChatGroq({
   model: "openai/gpt-oss-120b",
@@ -7,3 +8,23 @@ const groq = new ChatGroq({
   maxRetries: 2,
   // other params...
 });
+const gemini = new ChatGoogleGenerativeAI({
+  model: "gemini-2.5-flash",
+  temperature: 0,
+  maxRetries: 2,
+  // other params...
+});
+
+export const getModel = async (agent) => {
+  switch (agent) {
+    case "chat":
+      return groq;
+    case "search":
+      return groq;
+    case "coding":
+      return gemini;
+
+    default:
+      return groq;
+  }
+};
--- backend/services/agent/graph/router.js
+++ backend/services/agent/graph/router.js
@@ -1,1 +1,80 @@
-export const router = async (state) => {};
+import { getModel } from "../config/llmodels.js";
+
+export const router = async (state) => {
+  const llm = await getModel("router");
+
+  const prompt = `
+You are an intelligent router agent.
+
+Your job is to determine which specialized agent should handle the user's request.
+
+Available agents:
+
+- chat
+- search
+- coding
+- pdf
+- ppt
+- vission
+
+Routing Rules:
+
+chat:
+- General conversation
+- Greetings
+- Explanations
+- Learning
+- Questions
+- Casual discussion
+
+search:
+- Current events
+- Latest information
+- News
+- Real-time facts
+- Web search
+
+coding:
+- Generate code
+- Debug code
+- Build projects
+- Software architecture
+- API design
+- Programming questions
+
+pdf:
+- Extract information from PDF files
+- Summarize PDF files
+- Answer questions about PDF content
+
+ppt:
+- Generate PowerPoint presentations
+- Questions about presentations
+- PPT editing or slides
+
+vission:
+- Image analysis
+- OCR
+- Describe images
+- Answer questions about images
+
+Instructions:
+- Return ONLY one word.
+- Do not explain your answer.
+- Choose one of:
+chat
+search
+coding
+pdf
+ppt
+vission
+
+User Query:
+${state.prompt}
+`;
+
+  const response = await llm.invoke(prompt);
+  console.log(response);
+
+  return { agent: response.content.trim().toLowerCase() };
+};
